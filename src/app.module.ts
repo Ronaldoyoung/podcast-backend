5e61f0d2531644f780cb86ca249077ee04ccc8ae
ronaldoyoung <ronaldoyoung@gmail.com>
2021-12-30 13:01:42 +0900

    postgres install & configure install

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PodcastsModule } from './podcasts/podcasts.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'dooyoungyoun',
+      password: 'dbsendud12',
+      database: 'podcast',
+      synchronize: true,
+      logging: false,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
--- src/podcasts/podcasts.module.ts
+++ src/podcasts/podcasts.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PodcastsResolver } from './podcasts.resolver';
-// import { PodcastsController } from './podcasts.controller';
 import { PodcastsService } from './podcasts.service';
 
 @Module({
-  // controllers: [PodcastsController],
   providers: [PodcastsService, PodcastsResolver],
 })
 export class PodcastsModule {}
