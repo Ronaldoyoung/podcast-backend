7e880af7ddb27742444bf35ce7e089d5dbc17ef7
ronaldoyoung <ronaldoyoung@gmail.com>
2022-01-11 22:58:34 +0900

    toHavebeencalltimes error

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -76,8 +76,34 @@ describe('UserService', () => {
 
       expect(result).toEqual({ ok: true });
     });
+
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error());
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toEqual({ ok: false, error: 'Could not create user' });
+    });
   });
-  it.todo('login');
+  describe('login', () => {
+    const loginArgs = {
+      email: 'be@email.com',
+      password: 'be.password',
+    };
+    it('should fail if user not exists', async () => {
+      usersRepository.findOne.mockResolvedValue(null);
+      const result = await service.login(loginArgs);
+
+      expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(usersRepository.findOne).toHaveBeenCalledWith(
+        expect.any(Object),
+        expect.any(Object),
+      );
+
+      expect(result).toEqual({
+        ok: false,
+        error: 'User not found',
+      });
+    });
+  });
   it.todo('findById');
   it.todo('editProfile');
 });
